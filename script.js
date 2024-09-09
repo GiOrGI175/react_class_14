830e2b190bf8575f1d6035a18c68d0da46507df9
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-09-09 20:57:19 +0400

    destruct

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('hi');
+// const aphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// console.log(aphabet[0]);
+
+// console.log(aphabet[1]);
+
+// console.log(aphabet[2]);
+
+//! დესტრუქტორიზაცია
+
+// const [first, second, , four, ...rest] = aphabet;
+
+// console.log(first);
+
+// console.log(four);
+
+// console.log(rest);
+
+const aphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+const numbers = [1, 2, 3, 4, 5];
+
+const group3 = [...aphabet, ...numbers];
+
+console.log(group3);
